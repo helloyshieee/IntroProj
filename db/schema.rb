056f711b9bc971425786c4d3a650fddb0b5214d6
Ashley Villalobos <aashley.villalobos@outlook.com>
2023-02-10 10:26:11 -0600

    Created park_assets and park_spaces database

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,14 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_162309) do
+  create_table "park_assets", id: false, force: :cascade do |t|
+    t.integer "asset_id", null: false
+    t.string "asset_class"
+    t.string "park_name"
+    t.integer "park_id"
+    t.string "asset_type"
+    t.string "asset_size"
+    t.string "primary_field"
+  end
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_162036) do
   create_table "park_spaces", id: false, force: :cascade do |t|
     t.integer "park_id", null: false
     t.string "park_name"
@@ -26,6 +24,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_162036) do
     t.integer "water_area_in_hectares"
     t.integer "land_area_in_hectares"
   end
+
+  create_table "spray_pads", id: false, force: :cascade do |t|
+    t.string "park_name"
+    t.string "address"
+    t.string "is_open"
+    t.integer "complex_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'csv'
+require 'open-uri'
+require 'net/http'
 
 filename = Rails.root.join('db/spray_pads.csv')
 
@@ -18,4 +20,39 @@ spray_pads.each do |sp|
   address: sp["address"],
   is_open: sp["is_open"])
   puts sp
+end
+
+
+
+#Park Spaces
+
+url = 'https://data.winnipeg.ca/resource/tx3d-pfxq.json'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+spaces = JSON.parse(response)
+
+spaces.each do |s|
+  ParkSpace.find_or_create_by(park_id: s["park_id"], park_name: s["park_name"],
+  address: s["address"], location_description: s["location_description"],
+  park_category: s["park_category"], linear_park_system: s["linear_park_system"],
+  district: s["district:"], neighbourhood: s["neighbourhood"], electoral_ward: s["electoral_ward"], cca: s["cca"],
+  area_in_hectares: s["area_in_hectares"],
+  water_area_in_hectares: s["water_area_in_hectares"], land_area_in_hectares: s["land_area_in_hectares"])
+end
+
+
+
+#Asset
+
+url = 'https://data.winnipeg.ca/resource/dk7c-zxyd.json'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+park_a = JSON.parse(response)
+
+
+park_a.each do |assets_inventory|
+  ParkAsset.find_or_create_by(asset_id: assets_inventory["asset_id"], asset_class: assets_inventory["asset_class"],
+  park_name: assets_inventory["park_name"], park_id: assets_inventory["park_id"],
+  asset_type: assets_inventory["asset_type"], asset_size: assets_inventory["asset_size"],
+  primary_field: assets_inventory["primary_field"])
 end
